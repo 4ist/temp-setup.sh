4cd7858b429c7aa67536ddca482db37a3de8c695
Forrest Richardson <frichardson062@gmail.com>
2022-05-26 16:27:03 -0500

    init setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+packageManager=apt
+$packageManager install vim ranger tmux neofetch
+
+# prompt to install syncthing
+# Install syncthing
+# Connect to my home env somehow?
+# Pull Config/Notes/Todo (each optional) 
+
+# Rebind 
